1b6b9411b39fa2e48bc0a6c8a686dac97e859eb5
unknown <yanivsh@miglyanivsh.medcon.com>
2015-07-23 14:51:28 +0300

    second commit

--- yan.py
+++
@@ -1,9 +0,0 @@
-import urllib
-
-response = urllib.urlopen('http://eco99fm.maariv.co.il/jwplayer/AudioWindow.aspx?id=3198')
-html = response.read()
-print(html)
-
-local_file = open("jspro.htm", "w+")
-local_file.write(html)
-local_file.close()
